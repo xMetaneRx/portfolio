e6ddbe31809fcaa7719ae099baf25e41f6eae1e9
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-30 01:01:58 +0200

    Update the summary

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,8 +7,8 @@ const Contact = () => {
                 <div>
                     <h2 className="text-3xl my-2">Hi 👋, I'm Dawid.</h2>
                     <p className="text-sm my-2 text-neutral-300 leading-6">
-                        Hello there, I am an aspiring self-taught front-end developer. I 
-                        relish creating Web applications. I am currently developing my skills 
+                        Aspiring self-taught front-end developer. I relish 
+                        creating Web applications. I am currently developing my skills 
                         in JavaScript, Typescript, React, and Next.js
                     </p>
                 </div>
